350ac8e2b172160f400988032027baa9f13550d2
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2017-03-03 12:26:34 +0100

    note decodex originelle

--- fetch.py
+++ fetch.py
@@ -16,6 +16,7 @@ def slugify(value):
     return str(value)
 
 response = requests.get('https://docs.google.com/spreadsheets/export?id=1WJ1c9y8hHECdkVbBYULGR8XWrCv9YRtw2LoCM6LCAew&exportFormat=csv')
+response.encoding = 'UTF-8'
 assert response.status_code == 200, 'failed to download csv file'
 
 text_file = open('tmp.csv', 'w')
@@ -38,11 +39,17 @@ with open('tmp.csv', 'rb') as csvfile:
 
         entry = []
         note = 0
+        node_decodex = -1
 
         try:
             note = int(row[3])
         except:
             continue
+
+        try:
+            note_decodex = int(row[4])
+        except:
+            pass
 
         entry.append(note)
         entry.append(row[1])
@@ -53,7 +60,7 @@ with open('tmp.csv', 'rb') as csvfile:
         
         database['sites'][id] = entry
 
-        for i in range(4, len(row)-1):
+        for i in range(5, len(row)-1):
             if row[i]:
                 database['urls'][row[i]] = id
         
